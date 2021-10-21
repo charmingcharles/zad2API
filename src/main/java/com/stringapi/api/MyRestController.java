32d904581b40949f0a6e411b27cfbeaa910ef073
klesniewski50 <63211654+klesniewski50@users.noreply.github.com>
2021-10-21 13:09:59 +0200

    reszta projektu

--- src/main/java/com/stringapi/api/MyRestController.java
+++ src/main/java/com/stringapi/api/MyRestController.java
@@ -1,4 +1,13 @@
 package com.stringapi.api;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class MyRestController {
+
+    @RequestMapping(value = "/itself") //example: http://localhost:8080/itself?text=Karol
+    public String simpleExample(@RequestParam(value = "text", defaultValue = "Karol") String text) {
+        return text;
+    }
+
 }
